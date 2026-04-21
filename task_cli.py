5d8dd7f72839971efb9829c1e92f8476bd8c2357
david <davidacs10.08@gmail.com>
2026-04-20 22:14:08 -0400

    se agrego la funcionalidad para agregar una tarea y se acomodo el codigo para poder usar estas funciones

--- task_cli.py
+++ task_cli.py
@@ -1,14 +1,7 @@
 import sys
 import json
 import os
-
-
-if len(sys.argv) < 2:
-    print(f"Uso: python task_cli.py <command>")
-    sys.exit(1)
-else:
-    command = sys.argv[1]
-    print(f"Comando recibido: <{command}>")
+from datetime import datetime
 
 
 def load_tasks():
@@ -22,3 +15,40 @@ def load_tasks():
 def save_tasks(tasks):
     with open("tasks.json", "w") as f:
         json.dump(tasks, f, indent=4)
+
+
+def add_task(description):
+    tasks = load_tasks()
+    if tasks:
+        ids = []
+        for task in tasks:
+            ids.append(task["id"])
+        new_id = max(ids) + 1
+    else:
+        new_id = 1
+
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    new_task = {
+        "id": new_id,
+        "description": description,
+        "status": "todo",
+        "createdAt": now,
+        "updatedAt": now,
+    }
+    tasks.append(new_task)
+    save_tasks(tasks)
+    print(f"Tarea agregada exitosamente (ID: {new_id})")
+
+
+if len(sys.argv) < 2:
+    print(f"Uso: python task_cli.py <command>")
+    sys.exit(1)
+else:
+    command = sys.argv[1]
+
+    if command == "add":
+        if len(sys.argv) < 3:
+            print("Por favor, agregue una descripcion.")
+        else:
+            add_task(sys.argv[2])
