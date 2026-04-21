67006b48e70eb348f328a4983ae566a10109b998
david <davidacs10.08@gmail.com>
2026-04-20 21:05:45 -0400

    se agregaron funcionalidades para cargar tareas y para guardarlas

--- task_cli.py
+++ task_cli.py
@@ -1,4 +1,6 @@
 import sys
+import json
+import os
 
 
 if len(sys.argv) < 2:
@@ -7,3 +9,16 @@ if len(sys.argv) < 2:
 else:
     command = sys.argv[1]
     print(f"Comando recibido: <{command}>")
+
+
+def load_tasks():
+    if os.path.exists("tasks.json"):
+        with open("tasks.json", "r") as f:
+            content = json.load(f)
+            return content
+    return []
+
+
+def save_tasks(tasks):
+    with open("tasks.json", "w") as f:
+        json.dump(tasks, f, indent=4)
