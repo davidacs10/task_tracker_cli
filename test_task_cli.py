84712e6168765ea96937a5b050247597e3a3a412
david <davidacs10.08@gmail.com>
2026-04-22 15:57:08 -0400

    Se agregaron los primeros test para las funciones de agregar tareas

---
+++ test_task_cli.py
@@ -0,0 +1,43 @@
+import unittest
+from unittest.mock import patch
+from task_cli import *
+
+
+class TestAddTask(unittest.TestCase):
+    @patch("task_cli.save_tasks")
+    @patch("task_cli.load_tasks")
+    def test_add_first_task(self, mock_load, mock_save):
+        mock_load.return_value = []
+
+        add_task("Comprar leche")
+
+        saved_task = mock_save.call_args[0][0][0]
+
+        self.assertEqual(saved_task["id"], 1)
+        self.assertEqual(saved_task["description"], "Comprar leche")
+        self.assertEqual(saved_task["status"], "todo")
+
+    @patch("task_cli.save_tasks")
+    @patch("task_cli.load_tasks")
+    def test_add_second_task(self, mock_load, mock_save):
+        mock_load.return_value = [
+            {
+                "id": 1,
+                "description": "Tarea existente",
+                "status": "todo",
+                "createdAt": "2026-04-20 10:00:00",
+                "updatedAt": "2026-04-20 10:00:00",
+            }
+        ]
+
+        add_task("Estudiar")
+
+        saved_task = mock_save.call_args[0][0][1]
+
+        self.assertEqual(saved_task["id"], 2)
+        self.assertEqual(saved_task["description"], "Estudiar")
+        self.assertEqual(saved_task["status"], "todo")
+
+
+if __name__ == "__main__":
+    unittest.main()
